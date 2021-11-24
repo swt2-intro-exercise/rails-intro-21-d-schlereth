86d5e892d1782c7b516f5bfce00c32e61c61a02f
d-schlereth <avi1992@hotmail.de>
2021-11-24 13:16:22 +0100

    Adds New Author landing page

--- config/routes.rb
+++ config/routes.rb
@@ -3,18 +3,20 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rails routes".
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
+resources :author
+
   # You can have the root of your site routed with "root"
   root 'home#index'
 
   # Example resource route (maps HTTP verbs to controller actions automatically):
   #   resources :products
-  
+
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
   # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
-  
+
   # Example resource route with options:
   #   resources :products do
   #     member do
